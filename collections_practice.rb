a4f1fa2eeeab3504237a05fbf0480a7dfc24a402
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-04 19:11:36 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,3 +10,10 @@ end
 def sort_array_char_count(array)
   array.sort {|x,y| x.length <=> y.length}
 end
+
+def swap_elements(array)
+  temp = array[1]
+  array[1] = array[2]
+  array[2] = temp
+  array
+end
